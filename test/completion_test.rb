84c56a350c6be2abac16524e71d75456b2f9b642
Tyler <tyler.b.thrailkill@gmail.com>
2019-01-11 20:23:08 -0700

    Update line endings

--- test/completion_test.rb
+++ test/completion_test.rb
@@ -1,10 +1,10 @@
-require_relative "test_helper"
+require_relative 'test_helper'
 require_relative 'cli'
 
 class CompletionTest < Minitest::Test
   def test_cli_start_completion_generator
     ARGV.clear
-    ARGV << "generate_completions"
+    ARGV << 'generate_completions'
     CLI.start(ARGV)
   end
 
