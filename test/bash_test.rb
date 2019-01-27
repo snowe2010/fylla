2a14b6009153607fe7b6c95313ca757fb769474a
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2019-01-27 12:57:51 -0700

    Reenable accidentally disabled tests
    Fix issue with completion spacing

--- test/bash_test.rb
+++ test/bash_test.rb
@@ -1,36 +1,241 @@
 require_relative 'test_helper'
 require_relative 'bash_clis/cli'
 
-SUBCOMMAND = <<~HERE
-_foo()
+SINGLE_SUBCOMMAND = <<~HERE
+_test_plain ()
 {
-  local cur prev
-
-  cur=${COMP_WORDS[COMP_CWORD]}
-  prev=${COMP_WORDS[COMP_CWORD-1]}
-
-  case ${COMP_CWORD} in
-    1)
-      COMPREPLY=($(compgen -W "plain" -- ${cur}))
-    ;;
-    2)
-      case ${prev} in
-        configure)
-          COMPREPLY=($(compgen -W "CM DSP NPU" -- ${cur}))
-        ;;
-        show)
-          COMPREPLY=($(compgen -W "some other args" -- ${cur}))
-        ;;
-      esac
-    ;;
-    *)
-      COMPREPLY=()
-    ;;
-  esac
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W " --help -h" -- "$cur"))
 }
+_test_help ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W " --help -h" -- "$cur"))
+}
+_test() {
+  local i=1 subcommand_index
 
+  # find the subcommand
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+    test)
+      subcommand_index=$i
+      break
+      ;;
+    esac
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+          plain)
+            _test_plain
+            return ;;
+          help)
+            _test_help
+            return ;;
+      help)
+        COMPREPLY=""
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "plain help " -- "$cur"))
+}
 complete -F _test test
-  HERE
+HERE
+
+SUBCOMMAND_WITH_OPTIONS = <<~HERE
+_test_plain ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "--opt1 --help -h" -- "$cur"))
+}
+_test_help ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W " --help -h" -- "$cur"))
+}
+_test() {
+  local i=1 subcommand_index
+
+  # find the subcommand
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+    test)
+      subcommand_index=$i
+      break
+      ;;
+    esac
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+          plain)
+            _test_plain
+            return ;;
+          help)
+            _test_help
+            return ;;
+      help)
+        COMPREPLY=""
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "plain help " -- "$cur"))
+}
+complete -F _test test
+HERE
+
+
+NESTED_SUBCOMMAND_WITH_OPTIONS = <<~HERE
+_test_plain ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "--opt1 --help -h" -- "$cur"))
+}
+_test_subcommand_plain ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W " --help -h" -- "$cur"))
+}
+_test_subcommand_help ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W " --help -h" -- "$cur"))
+}
+_test_subcommand() {
+  local i=1 subcommand_index
+
+  # find the subcommand
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+    subcommand)
+      subcommand_index=$i
+      break
+      ;;
+    esac
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+          plain)
+            _test_subcommand_plain
+            return ;;
+          help)
+            _test_subcommand_help
+            return ;;
+      help)
+        COMPREPLY=""
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "plain help " -- "$cur"))
+}
+_test_subcommand2_plain ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "--opt1 --help -h" -- "$cur"))
+}
+_test_subcommand2_help ()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W " --help -h" -- "$cur"))
+}
+_test_subcommand2() {
+  local i=1 subcommand_index
+
+  # find the subcommand
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+    subcommand2)
+      subcommand_index=$i
+      break
+      ;;
+    esac
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+          plain)
+            _test_subcommand2_plain
+            return ;;
+          help)
+            _test_subcommand2_help
+            return ;;
+      help)
+        COMPREPLY=""
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "plain help " -- "$cur"))
+}
+_test() {
+  local i=1 subcommand_index
+
+  # find the subcommand
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+    test)
+      subcommand_index=$i
+      break
+      ;;
+    esac
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+          plain)
+            _test_plain
+            return ;;
+          subcommand)
+            _test_subcommand
+            return ;;
+          subcommand2)
+            _test_subcommand2
+            return ;;
+      help)
+        COMPREPLY=""
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "plain subcommand subcommand2 --class-opt" -- "$cur"))
+}
+complete -F _test test
+HERE
 
 class BashTest < Minitest::Test
   def setup
@@ -40,27 +245,27 @@ class BashTest < Minitest::Test
   def test_bash_single_subcommand
     ARGV.clear
     ARGV << 'generate'
-    expected = SUBCOMMAND
-    # assert_output(expected) do
+    expected = SINGLE_SUBCOMMAND
+    assert_output(expected) do
       CLI::Subcommand.start(ARGV)
-    # end
+    end
   end
 
   def test_bash_single_subcommand_with_options
     ARGV.clear
     ARGV << 'generate'
-    expected = SUBCOMMAND
-    # assert_output(expected) do
+    expected = SUBCOMMAND_WITH_OPTIONS
+    assert_output(expected) do
       CLI::SubcommandWithOptions.start(ARGV)
-    # end
+    end
   end
 
   def test_bash_nested_subcommand_with_options
     ARGV.clear
     ARGV << 'generate'
-    expected = SUBCOMMAND
-    # assert_output(expected) do
+    expected = NESTED_SUBCOMMAND_WITH_OPTIONS
+    assert_output(expected) do
       CLI::SubcommandWithNestedSubcommandsAndOptions.start(ARGV)
-    # end
+    end
   end
 end
